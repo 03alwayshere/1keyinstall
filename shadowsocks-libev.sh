12ba04d9d974b4e596c750488b015bd4f2d0450c
Kyo628 <51012973+Kyo628@users.noreply.github.com>
2020-05-28 17:14:18 +0800

    Update shadowsocks-libev.sh
    Default:chacha20-ietf-poly1305

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -300,7 +300,7 @@ pre_install(){
         hint="${ciphers[$i-1]}"
         echo -e "${green}${i}${plain}) ${hint}"
     done
-    read -p "Which cipher you'd select(Default: ${ciphers[0]}):" pick
+    read -p "Which cipher you'd select(Default: ${ciphers[13]}):" pick
     [ -z "$pick" ] && pick=1
     expr ${pick} + 1 &>/dev/null
     if [ $? -ne 0 ]; then
